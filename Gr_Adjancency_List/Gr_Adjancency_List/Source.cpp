babd2703ecf0d49b5a87911236138f474d5c1329
Prathamesh Mhatre <pmhatre1@asu.edu>
2021-07-06 23:58:59 -0700

    Algorithm

---
+++ Gr_Adjancency_List/Gr_Adjancency_List/Source.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void addEdge(vector<int> adj[], int u, int v)
+{
+	adj[u].push_back(v);
+	adj[v].push_back(u);
+}
+
+
+void printGraph(vector<int> adj[], int V)
+{
+	for (int v = 0; v < V; ++v)
+	{
+		cout << "\n Adjacency list of vertex "
+			<< v << "\n head ";
+		for (auto x : adj[v])
+			cout << "-> " << x;
+		printf("\n");
+	}
+}
+
+void main()
+{
+	
+	vector<int>adj[5];
+	addEdge(adj, 0, 1);
+	addEdge(adj, 0, 4);
+	addEdge(adj, 1, 2);
+	addEdge(adj, 1, 3);
+	addEdge(adj, 1, 4);
+	addEdge(adj, 2, 3);
+	addEdge(adj, 3, 4);
+	printGraph(adj, 5);
+}
